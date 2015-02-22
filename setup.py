9a3760a8f842e7367fc9afd1c284321d86eee200
Lubos Matl <matllubos@gmail.com>
2015-02-22 21:20:27 +0100

    v0.2.2 Add nose runner

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='germanium',
-    version='0.2.1',
+    version='0.2.2',
     description='Helpful methods for Python Selenium and REST testing',
     author='Lukas Rychtecky, Lubos Matl',
     author_email='lukas.rychtecky@gmail.com, matllubos@gmail.com',
