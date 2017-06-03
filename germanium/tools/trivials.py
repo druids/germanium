9e9e8dd460b66fd2ffeb5c68ac524db639eb0230
Luboš Mátl <matllubos@gmail.com>
2017-06-03 13:44:25 +0200

    2.0.2

--- germanium/tools/trivials.py
+++ germanium/tools/trivials.py
@@ -8,8 +8,8 @@ from nose.tools import (assert_equal, assert_true, assert_false, assert_in, asse
                         assert_not_equal, assert_is, assert_is_instance, assert_greater, assert_less,
                         assert_almost_equal, assert_not_almost_equal, assert_greater_equal, assert_less_equal,
                         assert_not_is_instance, assert_list_equal, assert_tuple_equal, assert_set_equal,
-                        assert_dict_equal, assert_sequence_equal, assert_multi_line_equal, assert_regex,
-                        assert_not_regex, assert_count_equal, assert_is_none, assert_is_not_none, assert_equals)
+                        assert_dict_equal, assert_sequence_equal, assert_multi_line_equal, assert_is_none,
+                        assert_is_not_none, assert_equals)
 
 
 def fail(msg=None):
