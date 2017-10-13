d4d92885408a4c8f502020066c567ff1713a04a9
Luboš Mátl <matllubos@gmail.com>
2017-10-13 16:18:10 +0200

    2.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='germanium',
-    version='2.0.3',
+    version='2.0.4',
     description='Helpful methods for Python Selenium and REST testing',
     author='Lukas Rychtecky, Lubos Matl',
     author_email='lukas.rychtecky@gmail.com, matllubos@gmail.com',
