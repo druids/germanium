7c6a3049a359d29be8244921c8f485ccc24f806e
Luboš Mátl <matllubos@gmail.com>
2021-04-01 18:29:58 +0200

    Added commit on callback helper

--- germanium/tools/trivials.py
+++ germanium/tools/trivials.py
@@ -3,12 +3,14 @@ Provides Nose and Django test case assert functions
 """
 from contextlib import contextmanager
 
-from nose.tools import (assert_equal, assert_true, assert_false, assert_in, assert_not_in, assert_raises,
-                        assert_not_equal, assert_is, assert_is_instance, assert_greater, assert_less,
-                        assert_almost_equal, assert_not_almost_equal, assert_greater_equal, assert_less_equal,
-                        assert_not_is_instance, assert_list_equal, assert_tuple_equal, assert_set_equal,
-                        assert_dict_equal, assert_sequence_equal, assert_multi_line_equal, assert_is_none,
-                        assert_is_not_none, assert_equals, assert_logs)
+from nose.tools import (
+    assert_equal, assert_true, assert_false, assert_in, assert_not_in, assert_raises, assert_not_equal, assert_is,
+    assert_is_instance, assert_greater, assert_less, assert_almost_equal, assert_not_almost_equal, assert_greater_equal,
+    assert_less_equal, assert_not_is_instance, assert_list_equal, assert_tuple_equal, assert_set_equal,
+    assert_dict_equal, assert_sequence_equal, assert_multi_line_equal, assert_is_none, assert_is_not_none,
+    assert_equals, assert_logs
+)
+from django_capture_on_commit_callbacks import capture_on_commit_callbacks
 
 from germanium import config
 
