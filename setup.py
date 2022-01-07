7a1435647362733343eb55074ecbf37d1c502447
Luboš Mátl <matllubos@gmail.com>
2022-01-07 15:46:01 +0100

    2.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-germanium',
-    version='2.3.0',
+    version='2.3.1',
     description='Helpful methods for Python Selenium and REST testing',
     author='Lukas Rychtecky, Lubos Matl',
     author_email='lukas.rychtecky@gmail.com, matllubos@gmail.com',
